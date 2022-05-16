84ca1327eddc1d50377b2c79d2b0364e2b98afe4
mochabbang <sungmin9036@gmail.com>
2022-05-17 06:14:55 +0900

    compositepattern-22.05.17

---
+++ composite_pattern/Menu.cs
@@ -0,0 +1,55 @@
+using Internal;
+using System;
+using System.ComponentModel.Design;
+namespace designpatterns.composite_pattern
+{
+    public class Menu: MenuComponent
+    {
+        List<MenuComponent> menuComponents = new List<MenuComponent>();
+        string name;
+        string description;
+
+        public Menu (string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public override void Add(MenuComponent menuComponent)
+        {
+            menuComponents.Add(menuComponent);
+        }
+
+        public override void Remove(MenuComponent menuComponent)
+        {
+            menuComponents.Remove(menuComponent);
+        }
+
+        public override MenuComponent GetChild(int i) 
+        {
+            return menuComponents[i];
+        }
+
+        public override string GetName()
+        {
+            return name;
+        }
+
+        public override string GetDescription()
+        {
+            return description;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("\n" + GetName());
+            Console.WriteLine(", " + GetDescription());
+            Console.WriteLine("----------------------");
+
+            foreach(MenuComponent menuComponent in menuComponents)
+            {
+                menuComponent.Print();
+            }
+        }
+    }
+}
---
+++ composite_pattern/MenuItem.cs
@@ -0,0 +1,51 @@
+using Internal;
+using System;
+namespace designpatterns.composite_pattern
+{
+    public class MenuItem: MenuComponent
+    {
+        string name;
+        string description;
+        bool vegetarian;
+        double price;
+
+        public MenuItem(string name, string description, bool vegetarian, double price)
+        {
+            this.name = name;
+            this.description = description;
+            this.vegetarian = vegetarian;
+            this.price = price;
+        }
+
+        public override string GetName()
+        {
+            return name;
+        }
+
+        public override string GetDescription()
+        {
+            return description;
+        }
+
+        public override double GetPrice()
+        {
+            return price;
+        }
+
+        public override bool IsVegetarian()
+        {
+            return vegetarian;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(" " + GetName());
+            if(IsVegetarian()) 
+            {
+                Console.WriteLine("(v)");
+            }
+            Console.WriteLine(", " + GetPrice());
+            Console.WriteLine("     -- " + GetDescription());
+        }
+    }
+}
